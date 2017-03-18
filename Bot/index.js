6465a246c13f068aad2762a3c90279b8b1b203ac
Carlous Fahmi <carlousf@hotmail.co.uk>
2017-03-18 16:57:19 +0000

    Finished questions 1-9

--- Bot/index.js
+++ Bot/index.js
@@ -1,28 +1,71 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+var annswerArray = []
+
 // Create bot and add dialogs
 var connector = new builder.ChatConnector({
     appId: "44126538-1649-4331-ae87-7c6fee6c4bb3",
     appPassword: "OgAE5bXq6knVGHMBqeGxUms"
 });
+
+var questions = [
+	"How often have you had little interest or pleasure in doing things?",
+	"How often have you been bothered by feeling down, depressed or hopeless?",
+	"How often have you been bothered by trouble falling or staying asleep, or sleeping too much?",
+	"How often have you been bothered by feeling tired or having little energy?",
+	"How often have you been bothered by poor appetite or overeating?",
+	"How often have you been bothered by feeling bad about yourself, or that you are a failure, or have let yourself or your family down?",
+	"How often have you been bothered by trouble concentrating on things, such as reading the newspaper or watching television?",
+	"How often have you been bothered by moving or speaking so slowly that other people could have noticed, or the opposite - being so fidgety or restless that you have been moving around a lot more than usual?",
+	"Have you had an anxiety attack (suddenly feeling fear or panic)?"
+]
+
+var answers = [
+	["Not at all", "Several days", "More than half the days", "Nearly every day"],
+	["Yes", "No"]
+]
+
+var responses = []
+
 var bot = new builder.UniversalBot(connector, [
     function (session) {
-        builder.Prompts.text(session, "Hello... What's your name?");
+        builder.Prompts.choice(session, questions[0], answers[0]);
     },
     function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+    	responses.push(results.response)
+        builder.Prompts.choice(session, questions[1],  answers[0]);
     },
     function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+    	responses.push(results.response)
+        builder.Prompts.choice(session, questions[2],  answers[0]);
     },
     function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                     " you've been programming for " + session.userData.coding + 
-                     " years and use " + session.userData.language + ".");
+    	responses.push(results.response)
+        builder.Prompts.choice(session, questions[3],  answers[0]);
+    },
+    function (session, results) {
+    	responses.push(results.response)
+        builder.Prompts.choice(session, questions[4],  answers[0]);
+    },
+    function (session, results) {
+    	responses.push(results.response)
+        builder.Prompts.choice(session, questions[5],  answers[0]);
+    },
+    function (session, results) {
+    	responses.push(results.response)
+        builder.Prompts.choice(session, questions[6],  answers[0]);
+    },
+    function (session, results) {
+    	responses.push(results.response)
+        builder.Prompts.choice(session, questions[7],  answers[0]);
+    },
+    function (session, results) {
+    	responses.push(results.response)
+        builder.Prompts.choice(session, questions[8],  answers[1]);
+    },
+    function (session, results) {
+        builder.Prompts.text(session, "The responses are: "+JSON.stringify(responses));
     }
 ]);
 
