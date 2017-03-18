7cbbc7eb4b73095a6deda3f559693453660afe46
Carlous Fahmi <carlousf@hotmail.co.uk>
2017-03-18 15:58:49 +0000

    new skeleton

--- Bot/index.js
+++ Bot/index.js
@@ -6,14 +6,29 @@ var connector = new builder.ChatConnector({
     appId: "44126538-1649-4331-ae87-7c6fee6c4bb3",
     appPassword: "OgAE5bXq6knVGHMBqeGxUms"
 });
-var bot = new builder.UniversalBot(connector);  
-bot.dialog('/', function (session) {
-    session.send('Hello World');
-});
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        builder.Prompts.text(session, "Hello... What's your name?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Got it... " + session.userData.name + 
+                     " you've been programming for " + session.userData.coding + 
+                     " years and use " + session.userData.language + ".");
+    }
+]);
 
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', connector.listen());
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
-});
+}); 
